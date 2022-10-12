2837d7768ec11f8c1697d1b2d868d2c3b7b3a65d
Valerio <canivalerio93@gmail.com>
2022-10-12 16:58:58 +0200

    inserite categories nella funzionalità edit

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -11,15 +11,30 @@
 
             <div class="form-group">
                 <label for="title">Title</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{$post->title}}">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title', $post->title)}}">
                 @error('title')
                     <div class="alert text-danger">{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group">
                 <label for="content">Content</label>
-                <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" cols="30" rows="3">{{$post->content}}</textarea>
+                <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" cols="30" rows="3">{{old('content', $post->content)}}</textarea>
                 @error('content')
+                    <div class="alert text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="category">Category</label>
+                <select class="form-control @error('category') is-invalid @enderror" name="category_id" id="category">
+
+                    <option {{(old('category_id')=='')?'selected':''}} value="">Nessuna categoria</option>
+                    @foreach ($categories as $category)
+                        <option  value="{{$category->id}}" {{(old('category_id', $post->category_id)==$category->id)?'selected':''}}>{{$category->name}}</option>
+                    @endforeach
+
+                </select>
+                 @error('category')
                     <div class="alert text-danger">{{ $message }}</div>
                 @enderror
             </div>
